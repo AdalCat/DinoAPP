9b11444551a92dd44c1759074de4342ed50285e7
Angel <angelSistemas21@gmail.com>
2022-12-08 08:23:59 -0600

    feat: Using fetch elements

--- src/components/Dinos.js
+++ src/components/Dinos.js
@@ -3,16 +3,16 @@ import React from 'react'
 const Dinos = ({ dinos = []}) => {
   return (
     <div className='row'>
-      {dinos.map((item, index) => (
-        <div key={index} className='col mb-5'>
-          <div className='card' style={{minWidth: "200px"}}>
-            <img src={item.image} alt='images'/>
-            <dv className="card-body">
+      {dinos.map((item) => (
+        <div key={item.id} className='col mb-5'>
+          <div className='card' style={{minWidth: '200px'}}>
+            <img src={item.img} alt={item.name}/>
+            <div className="card-body">
               <h4 className='card-title'>{item.name}</h4>
               <hr/>
-              <p>Habitat: {item.location.name}</p>
-              <p>Periodo: {item.origin.name}</p>
-            </dv>
+              <p>Habitat: {item.habitatPlace} - {item.habitatSubregion}</p>
+              <p>Periodo: {item.historicalperiodName} - {item.historicalperiodYear}</p>
+            </div>
           </div>
         </div>
       ))}
