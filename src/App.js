5603301f24de95e5129cf09dc3bcf6bed0c76aae
Angel <angelSistemas21@gmail.com>
2022-12-08 08:26:20 -0600

    feat: The getDinos function was created and the fetchDinos function was called

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
 import React, {useState, useEffect} from 'react'
 import Navbar from "./components/Navbar";
 import Dinos from "./components/Dinos";
-import Botonesnav from "./components/Botonesnav";
-
-
+import { fetchDinos } from './helpers/fetchDinos';
+// import Botonesnav from "./components/Botonesnav";
 
 function App() {
   const [dinos, setdinos] = useState([])
-  const [info, setinfo] = useState({})
-  const urlInicial = "https://rickandmortyapi.com/api/character"
+  // const [info, setinfo] = useState({})
 
-  const fetchDinos = (url) => {
-      fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        setdinos(data.results)
-        setinfo(data.info)
-      })
-      .catch(error => console.log(error))
-}
+// const onPrevious = () => {
+//   fetchDinos(info.prev)
+// }
 
-const onPrevious = () => {
-  fetchDinos(info.prev)
-}
+// const onNext = () => {
+//   fetchDinos(info.next)
+// }
 
-const onNext = () => {
-  fetchDinos(info.next)
+const getDinos = async () => {
+  const baseURL = 'http://localhost:3000/dinos';
+  const dinosaur = await fetchDinos(baseURL);
+  setdinos(dinosaur);
 }
 
 useEffect(() => {
-fetchDinos(urlInicial)
+  getDinos()
 }, [])
 
   return (
@@ -37,14 +31,12 @@ fetchDinos(urlInicial)
         <Navbar brand="DinoAPP"/>
 
         <div className="container mt-5">
-          <Botonesnav prev={info.prev} next={info.next} onPrevious={onPrevious} onNext={onNext}/>
+          {/* <Botonesnav prev={info.prev} next={info.next} onPrevious={onPrevious} onNext={onNext}/> */}
           <Dinos dinos={dinos}/>
-          <Botonesnav prev={info.prev} next={info.next} onPrevious={onPrevious} onNext={onNext}/>
+          {/* <Botonesnav prev={info.prev} next={info.next} onPrevious={onPrevious} onNext={onNext}/> */}
         </div>
     </>
   );
 }
-
-
 
 export default App;
