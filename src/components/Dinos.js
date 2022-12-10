e941f3fc2fff7bcfe0069268b0641ec77b5b03c3
Angel <angelSistemas21@gmail.com>
2022-12-10 14:55:32 -0600

    add: card-img and card-text

--- src/components/Dinos.js
+++ src/components/Dinos.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './../styles.css'
 
 const Dinos = ({ dinos = []}) => {
   return (
@@ -6,12 +7,12 @@ const Dinos = ({ dinos = []}) => {
       {dinos.map((item) => (
         <div key={item.id} className='col mb-5'>
           <div className='card' style={{minWidth: '200px'}}>
-            <img src={item.img} alt={item.name}/>
+            <img src={item.img} alt={item.name} className="card-img"/>
             <div className="card-body">
               <h4 className='card-title'>{item.name}</h4>
               <hr/>
-              <p>Habitat: {item.habitatPlace} - {item.habitatSubregion}</p>
-              <p>Periodo: {item.historicalperiodName} - {item.historicalperiodYear}</p>
+              <p className='card-text'>Habitat: {item.habitatPlace}-{item.habitatSubregion}</p>
+              <p className='card-text'>Periodo: {item.historicalperiodName}-{item.historicalperiodYear}</p>
             </div>
           </div>
         </div>
