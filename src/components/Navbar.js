aac39d373ebfa5f4d2aa008b69cab203cb896ef9
Alopez-7 <alejandro.lopezgzz7@gmail.com>
2022-12-14 12:21:05 -0600

    hashrouter hotfix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import  {Link}  from "react-router-dom";
 
 const Navbar = ({ brand }) => {
   return (
     <nav className="navbar navbar-dark bg-dark ">
     <div className="container justify-content-center">
       <a className="navbar-brand" href="https://adalcat.github.io/DinoAPP/"><img style={{width: "30px"}} alt="dino" src="favicon.png"/>{brand}</a>
-      <a href="/aboutapi" className="btn btn-info">Sobre la API</a>
+      <a  className="btn btn-info"><Link to="/aboutapi">Sobre la API</Link></a>
       <a href="/aboutus" className="btn btn-info">Los creadores</a>
     </div>
   </nav>
